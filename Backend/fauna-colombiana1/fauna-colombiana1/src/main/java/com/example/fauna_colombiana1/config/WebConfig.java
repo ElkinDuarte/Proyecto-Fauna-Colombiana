7dee9bd8a0969142c3b76b30df82ffb18f3854a9
ElkinDuarte <elkinfonseca32@gmail.com>
2024-11-16 12:42:15 -0500

    ConexionBackendyFrontend

---
+++ Backend/fauna-colombiana1/fauna-colombiana1/src/main/java/com/example/fauna_colombiana1/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.fauna_colombiana1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas del backend
+                .allowedOrigins("http://127.0.0.1:5500") // Permitir solicitudes desde tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(false); // Cambia a true si necesitas enviar cookies o autenticación
+    }
+}
