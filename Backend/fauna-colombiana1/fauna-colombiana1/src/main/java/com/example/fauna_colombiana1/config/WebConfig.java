feaff4e7903cf6280d8a24dbfa8a0f32ade834e4
ElkinDuarte <128104567+ElkinDuarte@users.noreply.github.com>
2024-11-29 20:38:08 -0500

    Update WebConfig.java

--- Backend/fauna-colombiana1/fauna-colombiana1/src/main/java/com/example/fauna_colombiana1/config/WebConfig.java
+++ Backend/fauna-colombiana1/fauna-colombiana1/src/main/java/com/example/fauna_colombiana1/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite todas las rutas del backend
-                .allowedOrigins("http://127.0.0.1:5500") // Permitir solicitudes desde tu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*") // Permitir todos los encabezados
-                .allowCredentials(false); // Cambia a true si necesitas enviar cookies o autenticación
+                .allowedOrigins("http://127.0.0.1:5500") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*") 
+                .allowCredentials(false); 
     }
 }
